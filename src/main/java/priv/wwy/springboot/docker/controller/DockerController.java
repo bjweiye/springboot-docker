e00d24804e005d3735afaaccb10ab159422fd0a8
wangweiye <wangweiye@wo.cn>
2021-09-17 21:36:50 +0800

    1st committing.

---
+++ src/main/java/priv/wwy/springboot/docker/controller/DockerController.java
@@ -0,0 +1,17 @@
+package priv.wwy.springboot.docker.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class DockerController {
+   // static Log log = LogFactory.getLog(DockerController.class);
+
+    @RequestMapping("/")
+    public String index() {
+        log.info("Hello Docker!");
+        return "Hello Docker!";
+    }
+}
